309f69cb636c54ae0f68753fb9d089f6333cd23e
ma7moudabozaid <ma7moudabozaid@gmail.com>
2023-01-07 13:16:35 +0200

    add yml file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Angular Ci Cd
+# Angular CI CD
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.3.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ng-app'),
+      dir: require('path').join(__dirname, './coverage/angular-ci-cd'),
       subdir: '.',
       reporters: [
         { type: 'html' },
